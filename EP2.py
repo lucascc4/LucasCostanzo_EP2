28daa3f28011394b36829ad337afcc60839264c9
lucascc4 <lucascc4@al.insper.edu.br>
2015-03-26 09:10:15 -0300

    commit 3

--- EP2.py
+++ EP2.py
@@ -28,3 +28,33 @@ t.right(90)
 t.forward(200)
 t.right(90)
 t.forward(100)
+t.right(90)	#cabeça
+t.circle(30)
+t.left(90)	#corpo
+t.penup()
+t.forward(60)
+t.pendown()
+t.forward(70)
+t.penup()	#braço1
+t.backward(60)
+t.left(45)
+t.pendown()
+t.forward(40)
+t.backward(40)
+t.right(90)	#braço2
+t.forward(40)
+t.backward(40)
+t.left(45)
+t.forward(60)
+t.left(45)	#perna1
+t.forward(40)
+t.backward(40)
+t.right(90)	#perna2
+t.forward(40)
+t.backward(40)
+t.left(45)
+window.mainloop()
+
+#importando palavras
+arquivo = open("texto.txt","r", encoding="utf-8")
+print(arquivo)
