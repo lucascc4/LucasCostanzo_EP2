5c601728b7406fc5730dfe061eeece47b2558ba7
lucascc4 <lucascc4@al.insper.edu.br>
2015-03-27 10:42:27 -0300

    commit 8

--- EP2.py
+++ EP2.py
@@ -13,140 +13,155 @@ from random import *
 window = turtle.Screen()    
 window.bgcolor("red")
 window.title("Jogo Da Forca")
-
 t = turtle.Turtle()  # Cria um objeto "desenhador"
-t.speed(30) 
-t.penup()       # Remova e veja o que acontece
-t.setpos(-250,-40)
-t.pendown()
-t.color("black")
-t.backward(100)
-t.forward(50)
-t.left(90)
-t.forward(300)
-t.right(90)
-t.forward(200)
-t.right(90)
-t.forward(100)
-
-
-
-def cabeça():
-	t.right(90)	#cabeça
-	t.circle(30)
-def corpo():
-	t.left(90)	#corpo
-	t.penup()
-	t.forward(60)
-	t.pendown()
-	t.forward(70)
-def braco1():
-	t.penup()	#braço1
-	t.backward(60)
-	t.left(45)
-	t.pendown()
-	t.forward(40)
-	t.backward(40)
-def braco2():
-	t.right(90)	#braço2
-	t.forward(40)
-	t.backward(40)
-	t.left(45)
-	t.forward(60)
-def perna1():
-	t.left(45)	#perna1
-	t.forward(40)
-	t.backward(40)
-def perna2():
-	t.right(90)	#perna2
-	t.forward(40)
-	t.backward(40)
-	t.left(45)
-
-#variavel_texto = window.textinput("Letra", "Escolha uma letra")	#caixa de texto
-
 t1 = turtle.Turtle()
-t1.speed(30) 
-t1.penup()      
-t1.setpos(-170,-70)
 
-p = open("texto.txt", encoding="utf-8")
-palavra = p.readlines()
-
-limpa = []
-for pa in palavra:
-	x = pa.strip().lower()
-	if x != "":
-		limpa.append(x)
-
-def traço(palavra):
-	for c in palavra:
-		if c ==" ":
-			t1.penup()
-			t1.forward(20)
-			t1.pendown()
-			
-		if c !=" ":
-			t1.pendown()
-			t1.forward(15)
-			t1.penup()
-			t1.forward(5)
+b = 0
+while b<1:
+	z = window.textinput("", "Você deseja jogar?").lower().strip()
+	if z == "sim":
 	
-print(limpa)		
-pc = choice(limpa)
-traço(pc)	#chama a funao de desenho da palavra
-erro = 0
-while erro < 6:
-	v = window.textinput("Letra", "Escolha uma letra").lower().strip()
-	if v in pc:
-		for i in range(len(pc)):
-			if v == pc[i]:
-				t1.penup()
-				t1.setpos(-170+20*i, -65)
-				t1.pendown()
-				t1.write(v)
-			elif v=="a" and pc[i] == "ã":
-				t1.penup()
-				t1.setpos(-170+20*i, -65)
-				t1.pendown()
-				t1.write(pc[i])
-			elif v=="o" and pc[i] == "ó":
-				t1.penup()
-				t1.setpos(-170+20*i, -65)
-				t1.pendown()
-				t1.write(pc[i])
-			elif v=="o" and pc[i] == "ô":
-				t1.penup()
-				t1.setpos(-170+20*i, -65)
-				t1.pendown()
-				t1.write(pc[i])
-			elif v=="i" and pc[i] == "í":
-				t1.penup()
-				t1.setpos(-170+20*i, -65)
-				t1.pendown()
-				t1.write(pc[i])
-	else:
-		erro +=1
-		if erro == 1:
-			cabeça()
-		if erro == 2:
-			corpo()
-		if erro == 3:
-			braco1()
-		if erro == 4:
-			braco2()
-		if erro == 5:
-			perna1()
-		if erro == 6:
-			perna2()
+		t.speed(30) 
+		t.penup()       # Remova e veja o que acontece
+		t.setpos(-250,-40)
+		t.pendown()
+		t.color("black")
+		t.backward(100)
+		t.forward(50)
+		t.left(90)
+		t.forward(300)
+		t.right(90)
+		t.forward(200)
+		t.right(90)
+		t.forward(100)
 		
-
-
+		
+		
+		def cabeça():
+			t.right(90)	#cabeça
+			t.circle(30)
+		def corpo():
+			t.left(90)	#corpo
+			t.penup()
+			t.forward(60)
+			t.pendown()
+			t.forward(70)
+		def braco1():
+			t.penup()	#braço1
+			t.backward(60)
+			t.left(45)
+			t.pendown()
+			t.forward(40)
+			t.backward(40)
+		def braco2():
+			t.right(90)	#braço2
+			t.forward(40)
+			t.backward(40)
+			t.left(45)
+			t.forward(60)
+		def perna1():
+			t.left(45)	#perna1
+			t.forward(40)
+			t.backward(40)
+		def perna2():
+			t.right(90)	#perna2
+			t.forward(40)
+			t.backward(40)
+			t.left(45)
+		
+		#variavel_texto = window.textinput("Letra", "Escolha uma letra")	#caixa de texto
+		
+		t1.speed(30) 
+		t1.penup()      
+		t1.setpos(-170,-70)
+		
+		p = open("texto.txt", encoding="utf-8")
+		palavra = p.readlines()
+		
+		limpa = []
+		for pa in palavra:
+			x = pa.strip().lower()
+			if x != "":
+				limpa.append(x)
+		
+		def traço(palavra):
+			for c in palavra:
+				if c ==" ":
+					t1.penup()
+					t1.forward(20)
+					t1.pendown()
+					
+				if c !=" ":
+					t1.pendown()
+					t1.forward(15)
+					t1.penup()
+					t1.forward(5)
+			
+		print(limpa)	
+		pc = choice(limpa)
+		traço(pc)	#chama a funao de desenho da palavra
+		erro = 0
+		acerto = 0
+		while erro < 6 and acerto < len(pc):
+			v = window.textinput("Letra", "Escolha uma letra").lower().strip()
+			if v in pc:
+				for i in range(len(pc)):
+					if v == pc[i]:
+						t1.penup()
+						t1.setpos(-170+20*i, -65)
+						t1.pendown()
+						t1.write(v)
+						acerto += 1
+					elif v=="a" and pc[i] == "ã":
+						t1.penup()
+						t1.setpos(-170+20*i, -65)
+						t1.pendown()
+						t1.write(pc[i])
+						acerto += 1
+					elif v=="o" and pc[i] == "ó":
+						t1.penup()
+						t1.setpos(-170+20*i, -65)
+						t1.pendown()
+						t1.write(pc[i])
+						acerto += 1
+					elif v=="o" and pc[i] == "ô":
+						t1.penup()
+						t1.setpos(-170+20*i, -65)
+						t1.pendown()
+						t1.write(pc[i])
+						acerto += 1
+					elif v=="i" and pc[i] == "í":
+						t1.penup()
+						t1.setpos(-170+20*i, -65)
+						t1.pendown()
+						t1.write(pc[i])
+						acerto += 1
+			else:
+				erro +=1
+				if erro == 1:
+					cabeça()
+				if erro == 2:
+					corpo()
+				if erro == 3:
+					braco1()
+				if erro == 4:
+					braco2()
+				if erro == 5:
+					perna1()
+				if erro == 6:
+					perna2()
+					
+		z = window.textinput("", "Você deseja jogar novamente?").lower().strip()
+		
+		if z == "nao":
+			b += 1
+			
+		
 window.mainloop()
-
-
-
-			
-		
-
 	
+	
+	
+				
+			
+	
+		
